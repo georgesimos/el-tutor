d5414072a2e4ce260fdcbf4cabccd00df00ec0d4
George <admin@georgesimos.com>
2020-01-05 18:10:56 +0200

    comments

--- server/config/auth.js
+++ server/config/auth.js
@@ -6,7 +6,9 @@ const User = require('../models/User');
 
 const auth = async (req, res, next) => {
   try {
+    // Get token from header
     const token = req.header('Authorization').replace('Bearer ', '');
+    // Verify token
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     const user = await User.findOne({
       _id: decoded._id,
