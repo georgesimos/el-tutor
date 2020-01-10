1590240b134df741f68a19254a15237785779585
George <admin@georgesimos.com>
2020-01-10 17:43:35 +0200

    students :hammer: only admin

--- server/routes/api/students.js
+++ server/routes/api/students.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 const auth = require('../../config/auth');
+const isAdmin = require('../../utils/isAdmin');
 const Student = require('../../models/Student');
 
 /* Get all students */
-router.get('/', auth, async (req, res) => {
+router.get('/', auth, isAdmin, async (req, res) => {
   try {
     const students = await Student.find({}).populate('_user', ['name', 'email']);
     res.send(students);
@@ -13,7 +14,7 @@ router.get('/', auth, async (req, res) => {
 });
 
 /* Get student by id */
-router.get('/:id', auth, async (req, res) => {
+router.get('/:id', auth, isAdmin, async (req, res) => {
   try {
     const { id } = req.params;
     const student = await Student.findById(id);
