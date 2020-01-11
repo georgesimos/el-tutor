a7735c42b6e4da46021294f491d2ae5d00e9e5d1
George <admin@georgesimos.com>
2020-01-11 16:17:05 +0200

    index for students store

--- client/src/store/actions/index.js
+++ client/src/store/actions/index.js
@@ -1,3 +1,4 @@
 export * from './alert';
 export * from './auth';
 export * from './users';
+export * from './lessons';
--- client/src/store/reducers/index.js
+++ client/src/store/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import alert from './alert';
 import auth from './auth';
 import users from './users';
+import lessons from './lessons';
 
 export default combineReducers({
   alertState: alert,
   authState: auth,
   userState: users,
+  lessonState: lessons
 });
