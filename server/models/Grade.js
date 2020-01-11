66c76938685ca770a5082dcd4c58b8c904036c35
George <admin@georgesimos.com>
2020-01-11 16:16:17 +0200

    adding timestamps

--- server/models/Grade.js
+++ server/models/Grade.js
@@ -2,11 +2,14 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
-const gradeSchema = Schema({
-  grade: { type: Number, required: true },
-  _lesson: { type: Schema.Types.ObjectId, ref: 'Lesson' },
-  _student: { type: Schema.Types.ObjectId, ref: 'Student' }
-});
+const gradeSchema = Schema(
+  {
+    grade: { type: Number, required: true },
+    _lesson: { type: Schema.Types.ObjectId, ref: 'Lesson' },
+    _student: { type: Schema.Types.ObjectId, ref: 'Student' }
+  },
+  { timestamps: true }
+);
 
 const Grade = mongoose.model('Grade', gradeSchema);
 module.exports = Grade;
--- server/models/Lesson.js
+++ server/models/Lesson.js
@@ -2,12 +2,15 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
-const lessonSchema = Schema({
-  title: { type: String, required: true },
-  description: String,
-  _teacher: { type: Schema.Types.ObjectId, ref: 'Teacher' },
-  _students: [{ type: Schema.Types.ObjectId, ref: 'Student' }]
-});
+const lessonSchema = Schema(
+  {
+    title: { type: String, required: true },
+    description: String,
+    _teacher: { type: Schema.Types.ObjectId, ref: 'Teacher' },
+    _students: [{ type: Schema.Types.ObjectId, ref: 'Student' }]
+  },
+  { timestamps: true }
+);
 
 const Lesson = mongoose.model('Lesson', lessonSchema);
 module.exports = Lesson;
