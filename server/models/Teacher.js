9bd467dc8019baeb3fc57f9ea025e5ffd813c506
George <admin@georgesimos.com>
2020-01-12 13:12:27 +0200

    grade ref

--- server/models/Teacher.js
+++ server/models/Teacher.js
@@ -4,7 +4,8 @@ const { Schema } = mongoose;
 
 const teacherSchema = Schema({
   _user: { type: Schema.Types.ObjectId, ref: 'User' },
-  lesson: { type: Schema.Types.ObjectId, ref: 'Lesson' }
+  _lesson: { type: Schema.Types.ObjectId, ref: 'Lesson' },
+  _grades: [{ type: Schema.Types.ObjectId, ref: 'Grade' }]
 });
 
 const Teacher = mongoose.model('Teacher', teacherSchema);
