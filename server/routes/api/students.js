353b26e0fee5824d20fb113e3d11af4e8ff44832
George <admin@georgesimos.com>
2020-01-13 21:11:36 +0200

    student profile router

--- server/routes/api/students.js
+++ server/routes/api/students.js
@@ -14,8 +14,31 @@ router.get('/', auth, isAdmin, async (req, res) => {
   }
 });
 
+/* Get student profile */
+router.get('/me', auth, isStudent, async (req, res) => {
+  console.log(req);
+  console.log(req.user);
+  const { _student, role } = req.user;
+  if (role !== 'student')
+    res.status(401).send({ error: 'forbidden, this endpoint is only for students' });
+  try {
+    const student = await await Student.findById(_student)
+      .populate({
+        path: '_grades',
+        populate: { path: '_lesson', populate: { path: '_teacher', populate: '_user' } }
+      })
+      .populate({ path: '_grades', populate: { path: '_student', populate: '_user' } })
+      .populate({ path: '_lessons', populate: { path: '_teacher', populate: '_user' } })
+      .populate('_user', ['name', 'email']);
+
+    return !student ? res.sendStatus(404) : res.send(student);
+  } catch (e) {
+    return res.sendStatus(400);
+  }
+});
+
 /* Get student by id */
-router.get('/only/:id', auth, isStudent, async (req, res) => {
+router.get('/:id', auth, isStudent, async (req, res) => {
   try {
     const { id } = req.params;
     const student = await await Student.findById(id)
