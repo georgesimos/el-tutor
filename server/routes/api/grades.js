2ed701b7e16d81e2380c376facb2b132e06c31d2
George <admin@georgesimos.com>
2020-01-13 21:12:17 +0200

    saving grades to teacher and student array

--- server/routes/api/grades.js
+++ server/routes/api/grades.js
@@ -5,6 +5,8 @@ const isTeacher = require('../../utils/isTeacher');
 const isStudent = require('../../utils/isStudent');
 const Grade = require('../../models/Grade');
 const Lesson = require('../../models/Lesson');
+const Teacher = require('../../models/Teacher');
+const Student = require('../../models/Student');
 
 /* Get all grades */
 router.get('/', auth, isAdmin, async (req, res) => {
@@ -71,12 +73,21 @@ router.post('/', auth, isTeacher, async (req, res) => {
   const { grade, _student, _lesson } = req.body;
   const lesson = await Lesson.findById(_lesson); // find the specific lesson
 
-  if (req.user.role !== 'admin' && lesson._teacher !== req.user._id)
-    return res.status(401).send({ error: 'This lesson is not yours...' });
+  if (req.user.role !== 'admin') {
+    if (JSON.stringify(lesson._teacher) !== JSON.stringify(req.user._teacher))
+      return res.status(401).send({ error: 'This lesson is not yours...' });
+  }
+  const teacher = await Teacher.findById(lesson._teacher);
+  const student = await Student.findById(_student);
 
   const newGrade = new Grade({ grade, _student, _lesson });
+  teacher._grades.push(newGrade._id);
+  student._grades.push(newGrade._id);
+
   try {
     await newGrade.save();
+    await teacher.save();
+    await student.save();
     return res.send(newGrade);
   } catch (e) {
     return res.status(400).send(e);
