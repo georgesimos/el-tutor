d3e991ac73da9b9b47e11b2c6e437ae2003f5093
George <admin@georgesimos.com>
2020-01-13 21:13:31 +0200

    reducer

--- client/src/store/reducers/profile.js
+++ client/src/store/reducers/profile.js
@@ -1,7 +1,8 @@
-import { GET_STUDENT_PROFILE } from '../types';
+import { GET_STUDENT_PROFILE, GET_TEACHER_PROFILE } from '../types';
 
 const initialState = {
-  student: null
+  student: null,
+  teacher: null
 };
 
 const getStudentProfile = (state, payload) => ({
@@ -9,11 +10,18 @@ const getStudentProfile = (state, payload) => ({
   student: payload
 });
 
+const getTeacherProfile = (state, payload) => ({
+  ...state,
+  teacher: payload
+});
+
 export default (state = initialState, action) => {
   const { type, payload } = action;
   switch (type) {
     case GET_STUDENT_PROFILE:
       return getStudentProfile(state, payload);
+    case GET_TEACHER_PROFILE:
+      return getTeacherProfile(state, payload);
     default:
       return state;
   }
